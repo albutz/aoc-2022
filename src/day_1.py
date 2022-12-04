dd05ae650554615384b6bd6de305f8c967fafb9f
Alexander Butz <alex.butz@gmail.com>
2022-12-04 18:40:15 +0100

    refactor: solution for day 1

--- src/day_1.py
+++ src/day_1.py
@@ -1,35 +1,33 @@
 """Advent of Code - Day 1."""
 from pathlib import Path
+from typing import List
 
 FILE_PATH = Path(__file__).parent.parent / "data" / "day_1.txt"
 
 
-def find_elves_most_calories(calories: Path | str, n: int) -> int:
+def find_elves_most_calories(calories: List, n: int) -> int:
     """Fat elf detection.
 
     Find the elf carrying the most calories.
 
     Args:
-        calories: Input file for puzzle.
+        calories: Calorie list for puzzle.
         n: Threshold for top n elves by calories.
 
     Returns:
         int: Total calories for top n elves.
     """
-    with open(calories, "r") as file:
-        lines = [line.strip() for line in file.readlines()]
-
-    line_breaks = [int(i) for i, line in enumerate(lines) if line == ""]
+    line_breaks = [int(i) for i, line in enumerate(calories) if line == ""]
 
     # inner groups
     elves = [
-        lines[(line_breaks[i] + 1) : line_breaks[i + 1]]
+        calories[(line_breaks[i] + 1) : line_breaks[i + 1]]
         for i, _ in enumerate(line_breaks[:-1])
     ]
 
     # outer groups
-    elves.insert(0, lines[: line_breaks[0]])
-    elves.append(lines[(line_breaks[-1] + 1) :])
+    elves.insert(0, calories[: line_breaks[0]])
+    elves.append(calories[(line_breaks[-1] + 1) :])
 
     # Sum per elf (tuple with elf number first, sum of calories second)
     cals = [(num + 1, sum([int(cal) for cal in elf])) for num, elf in enumerate(elves)]
@@ -41,5 +39,8 @@ def find_elves_most_calories(calories: Path | str, n: int) -> int:
 
 
 if __name__ == "__main__":
-    print(f"Part 1: {find_elves_most_calories(calories=FILE_PATH, n=1)}.")
-    print(f"Part 2: {find_elves_most_calories(calories=FILE_PATH, n=3)}.")
+    with open(FILE_PATH, "r") as file:
+        calories = [line.strip() for line in file.readlines()]
+
+    print(f"Part 1: {find_elves_most_calories(calories, n=1)}.")
+    print(f"Part 2: {find_elves_most_calories(calories, n=3)}.")
